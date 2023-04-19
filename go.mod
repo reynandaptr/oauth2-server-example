a1bf6e048076572920ce891ebcb470d53be26dff
reynandaptr <reynandapp1997@gmail.com>
2023-04-19 08:25:43 +0700

    feat: initial commit

---
+++ go.mod
@@ -0,0 +1,36 @@
+module github.com/reynandaptr/oauth2-playground
+
+go 1.20
+
+require (
+	github.com/google/uuid v1.3.0
+	github.com/jackc/pgx/v4 v4.18.1
+	github.com/sirupsen/logrus v1.9.0
+	github.com/vgarvardt/go-oauth2-pg/v3 v3.3.2
+	github.com/vgarvardt/go-pg-adapter v1.0.0
+	gopkg.in/oauth2.v3 v3.12.0
+)
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.2 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
+	github.com/jackc/puddle v1.3.0 // indirect
+	github.com/jmoiron/sqlx v1.2.0 // indirect
+	github.com/sergi/go-diff v1.1.0 // indirect
+	github.com/tidwall/buntdb v1.1.2 // indirect
+	github.com/tidwall/gjson v1.12.1 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/valyala/fasthttp v1.34.0 // indirect
+	github.com/vgarvardt/pgx-helpers/v4 v4.0.0-20200225100150-876aee3d1a22 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v4"
+	"github.com/sirupsen/logrus"
+	pg "github.com/vgarvardt/go-oauth2-pg/v3"
+	"github.com/vgarvardt/go-pg-adapter/pgx4adapter"
+	"gopkg.in/oauth2.v3/manage"
+	"gopkg.in/oauth2.v3/models"
+
+	"gopkg.in/oauth2.v3/errors"
+	"gopkg.in/oauth2.v3/server"
+)
+
+func main() {
+	pgxConn, err := pgx.Connect(context.TODO(), os.Getenv("DB_URI"))
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	manager := manage.NewDefaultManager()
+
+	// use PostgreSQL token store with pgx.Connection adapter
+	adapter := pgx4adapter.NewConn(pgxConn)
+	tokenStore, _ := pg.NewTokenStore(adapter, pg.WithTokenStoreGCInterval(time.Minute))
+	defer tokenStore.Close()
+
+	clientStore, _ := pg.NewClientStore(adapter)
+
+	manager.MapTokenStorage(tokenStore)
+	manager.MapClientStorage(clientStore)
+
+	srv := server.NewDefaultServer(manager)
+	srv.SetAllowGetAccessRequest(true)
+	srv.SetClientInfoHandler(server.ClientBasicHandler)
+
+	srv.SetUserAuthorizationHandler(func(w http.ResponseWriter, r *http.Request) (userID string, err error) {
+		return "1", nil
+	})
+
+	manager.SetRefreshTokenCfg(manage.DefaultRefreshTokenCfg)
+
+	srv.SetInternalErrorHandler(func(err error) (re *errors.Response) {
+		log.Println("Internal Error:", err.Error())
+		return
+	})
+
+	srv.SetResponseErrorHandler(func(re *errors.Response) {
+		log.Println("Response Error:", re.Error.Error())
+	})
+
+	http.HandleFunc("/token", func(w http.ResponseWriter, r *http.Request) {
+		srv.HandleTokenRequest(w, r)
+	})
+
+	http.HandleFunc("/auth", func(w http.ResponseWriter, r *http.Request) {
+		srv.HandleAuthorizeRequest(w, r)
+	})
+
+	http.HandleFunc("/credentials", func(w http.ResponseWriter, r *http.Request) {
+		clientId := uuid.New().String()[:8]
+		clientSecret := uuid.New().String()[:8]
+		err := clientStore.Create(&models.Client{
+			ID:     clientId,
+			Secret: clientSecret,
+			Domain: "http://localhost:9094",
+			UserID: "1",
+		})
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"client_id": clientId, "client_secret": clientSecret})
+	})
+
+	http.HandleFunc("/protected", validateToken(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, I'm protected"))
+	}, srv))
+
+	log.Fatal(http.ListenAndServe(":9096", nil))
+}
+
+func validateToken(f http.HandlerFunc, srv *server.Server) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, err := srv.ValidationBearerToken(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		f.ServeHTTP(w, r)
+	})
+}
